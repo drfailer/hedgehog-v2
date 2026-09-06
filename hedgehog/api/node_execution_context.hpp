13e2ebd65aec2a2c3400264e89c23939a5296e85
drfailer <drfailer.42@gmail.com>
2026-09-06 11:00:34 -0400

    add a profiler in the execution context

--- hedgehog/api/node_execution_context.hpp
+++ hedgehog/api/node_execution_context.hpp
@@ -25,6 +25,7 @@ template <typename NodeType>
 class NodeExecutionContext {
     NodeType *node_;
     RuntimeInfo info_;
+    Profiler profiler_;
 
   public:
     // constructor used by the node
@@ -35,6 +36,7 @@ class NodeExecutionContext {
 
     NodeType &node() { return *node_; }
     RuntimeInfo const &info() { return info_; }
+    Profiler &profiler() { return profiler_; }
 
     std::string const &name() { return info_.node.name; }
     std::string const &graph_name() { return info_.graph.name; }
